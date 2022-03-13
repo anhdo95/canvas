ce434b8200d725b29542f90fe988fbae1bc2d7bc
Anh Do <do.dinh.anh@linecorp.com>
2022-03-13 22:30:00 +0700

    Show player and background

--- game/script.js
+++ game/script.js
@@ -3,10 +3,11 @@
 window.onload = startGame
 
 /** @type {Component} */
-let gamePiece, gameScore, gameObstacles = []
+let player = getPlayer(), background = getBackground(), gamePiece, gameScore, gameObstacles = []
 
 function startGame() {
-  gamePiece = new Component(10, 120, 30, 30, "red")
+  gamePiece = new ImageComponent(10, 120, 30, 30, player.smiley)
+  gameBackground = new BackgroundComponent(0, 250, 480, 312, 0, 0, 480, 312, background)
   gameScore = new TextComponent(340, 30, '#f50', '18px Arial', 'SCORE: 0')
   gameArea.start()
 }
@@ -42,10 +43,20 @@ const gameArea = {
   handleKeyUp(event) {
     if (!this.isValidKey(event.key)) return
     this.keys[event.key] = false
+
+    if (
+      !gameArea.keys["ArrowUp"] &&
+      !gameArea.keys["ArrowDown"] &&
+      !gameArea.keys["ArrowLeft"] &&
+      !gameArea.keys["ArrowRight"]
+    ) {
+      gamePiece.image = player.smiley;
+    }
   },
   handleKeyDown(event) {
     if (!this.isValidKey(event.key)) return
     this.keys[event.key] = true
+    gamePiece.image = player.angry
   }
 }
 
@@ -64,13 +75,17 @@ function updateGameArea() {
   if (gameArea.keys['ArrowLeft']) gamePiece.speedX--
   if (gameArea.keys['ArrowRight']) gamePiece.speedX++
 
+  gameBackground.x--
+  gameBackground.moveToNewPosition()
+  gameBackground.update()
+
   gameArea.frameNo++
   if (gameArea.frameNo === 1 || everyInterval(150)) {
     const x         = gameArea.canvas.width
           minHeight = 30,
           maxHeight = 200,
           height    = Math.floor(Math.random() * (maxHeight - minHeight) + minHeight),
-          minGap    = 30,
+          minGap    = 50,
           maxGap    = 150,
           gap       = Math.floor(Math.random() * (maxGap - minGap) + minGap)
 
@@ -150,6 +165,61 @@ function TextComponent(x, y, color, font, text) {
     ctx.fillStyle = this.color
     ctx.fillText(this.text, this.x, this.y)
   }
+}
+
+function ImageComponent(x, y, width, height, image) {
+  const component = new Component(x, y, width, height)
+  component.image = image
+  
+  component.update = function() {
+    if (this.image) {
+      const ctx = gameArea.context
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+  }
+  return component
+}
+
+function BackgroundComponent(sx, sy, sw, sh, x, y, width, height, image) {
+  const component = new Component(x, y, width, height)
+  component.sx = sx
+  component.sy = sy
+  component.sw = sw
+  component.sh = sh
+  component.image = image
+
+  component.moveToNewPosition = function() {
+    this.x += this.speedX;
+
+    if (this.x === -this.width) {
+      this.x = 0
+    }
+  }
+  
+  component.update = function() {
+    if (this.image) {
+      const ctx = gameArea.context
+      ctx.drawImage(this.image, this.sx, this.sy, this.sw, this.sh, this.x, this.y, this.width, this.height)
+      ctx.drawImage(this.image, this.sx + this.width, this.sy, this.sw, this.sh, this.x + this.width, this.y, this.width, this.height)
+    }
+  }
+  return component
+}
+
+function getPlayer() {
+  const smiley = new Image()
+  smiley.src = 'images/smiley.gif'
+  const angry = new Image()
+  angry.src = 'images/angry.gif'
+
+  return { smiley, angry } 
+}
+
+function getBackground() {
+  const background = new Image()
+  background.src = 'images/background.jpeg'
+
+  return background
 }
 
 function everyInterval(n) {
