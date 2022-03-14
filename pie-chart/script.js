fce08b7927ef104404d369047078112d5d1d4670
Anh Do <do.dinh.anh@linecorp.com>
2022-03-14 22:18:33 +0700

    Show labels

--- pie-chart/script.js
+++ pie-chart/script.js
@@ -32,7 +32,7 @@ function PieChart(options) {
   this.data = options.data
   this.colors = options.colors
 
-  this.draw = function() {
+  this.draw = function(offset = 0) {
     const total = Object.values(this.data).reduce((sum, value) => sum + value)
 
     let startAngle = 0,
@@ -40,15 +40,29 @@ function PieChart(options) {
     Object.values(this.data).forEach((value) => {
       // Full circle corresponds to an angle of 360 degrees or 2*PI
       const sliceAngle = Math.PI * 2 * value / total
+      const pieRadius = Math.min(this.canvas.width / 2, this.canvas.height / 2)
+      const endAngle = startAngle + sliceAngle
   
       drawPieSlice(
         this.ctx,
         this.canvas.width / 2,
         this.canvas.height / 2,
-        Math.min(this.canvas.width / 2, this.canvas.height / 2),
+        pieRadius,
         startAngle,
-        startAngle + sliceAngle,
+        endAngle,
         this.colors[colorIndex++ % this.colors.length]
+      )
+
+      let labelX = this.canvas.width / 2 + (offset + pieRadius / 2) * Math.cos(startAngle + sliceAngle / 2)
+      let labelY = this.canvas.height / 2 + (offset + pieRadius / 2) * Math.sin(startAngle + sliceAngle / 2)
+
+      drawText(
+        this.ctx,
+        labelX,
+        labelY,
+        `${Math.round(100 * value / total)}%`,
+        'bold 18px Arial',
+        '#fff'
       )
 
       startAngle += sliceAngle
@@ -65,13 +79,15 @@ function DoughnutChart(options) {
   this.base = new PieChart(options)
 
   this.draw = function() {
-    this.base.draw()
+    const radius = Math.min(this.base.canvas.width / 2, this.base.canvas.height / 2) * this.doughnutHoleSize
+    const offset = radius / 2
+    this.base.draw(offset)
 
     drawPieSlice(
       this.base.ctx,
       this.base.canvas.width / 2,
       this.base.canvas.height / 2,
-      this.doughnutHoleSize * Math.min(this.base.canvas.width / 2, this.base.canvas.height / 2),
+      radius,
       0,
       2 * Math.PI,
       '#fff'
@@ -124,3 +140,20 @@ function drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, color
   ctx.closePath
   ctx.fill()
 }
+
+/**
+ * @param {CanvasRenderingContext2D} ctx 
+ * @param {number} centerX 
+ * @param {number} centerY 
+ * @param {number} radius 
+ * @param {number} startAngle 
+ * @param {number} endAngle 
+ * @param {string} color 
+ */
+ function drawText(ctx, x, y, text, font, color) {
+  ctx.font = font
+  ctx.fillStyle = color
+  ctx.beginPath()
+  ctx.fillText(text, x, y)
+}
+
